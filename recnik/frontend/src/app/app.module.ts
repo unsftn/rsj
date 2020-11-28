8dbe3b3db19aaa0c6b7551dd6b7b60f6491f5196
Branko Milosavljevic <mbranko@uns.ac.rs>
2020-11-28 16:19:46 +0100

    Update auth servisa

--- recnik/frontend/src/app/app.module.ts
+++ recnik/frontend/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { PanelModule } from 'primeng/panel';
+import { authInterceptorProviders } from './services/auth/auth-interceptor';
 
 import { AppComponent } from './app.component';
+
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   declarations: [
@@ -15,11 +19,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     PanelModule,
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
