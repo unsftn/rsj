8f4215793abd28104a1ac22104e16cd5e74772ad
Branko Milosavljevic <mbranko@uns.ac.rs>
2020-12-05 21:53:57 +0100

    URL fix

--- recnik/backend/recnik/urls.py
+++ recnik/backend/recnik/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/publikacije/', include('publikacije.urls', namespace='publikacije')),
-    path(r'swagger(<str:format>\.json|\.yaml)/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
