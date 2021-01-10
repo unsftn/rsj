b20b685a28ea6ce07b26d5bcc4075b85b5797a75
milanatucakov <57504983+milanatucakov@users.noreply.github.com>
2021-01-10 20:02:03 +0100

    Link keywords with BE and add save button

--- recnik/frontend/src/app/pages/expressions/expressions.component.ts
+++ recnik/frontend/src/app/pages/expressions/expressions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'expressions',
@@ -7,7 +8,10 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./expressions.component.scss'],
 })
 export class ExpressionsComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private httpClient: HttpClient,
+  ) {}
 
   keyWords = [];
   selectedKeyWord: string;
@@ -17,13 +21,20 @@ export class ExpressionsComponent implements OnInit {
     this.expressions.push({ value: '', keyWord: this.keyWords[0] });
   }
 
+  async fetch() {
+    const response: any = await this.httpClient
+      .get('api/odrednice/odrednica')
+      .toPromise();
+
+    if (response) {
+      this.keyWords = response.results.map((item) => {
+        return item.rec;
+      });
+    }
+  }
+
   ngOnInit() {
     this.primengConfig.ripple = true;
-    this.keyWords = [
-      'Кључна реч 1',
-      'Кључна реч 2',
-      'Кључна реч 3',
-      'Кључна реч 4',
-    ];
+    this.fetch();
   }
 }
