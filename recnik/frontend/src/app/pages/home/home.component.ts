f221dd829ff889f848dc70b65fd406e2b68234ac
milanatucakov <57504983+milanatucakov@users.noreply.github.com>
2021-01-10 15:18:48 +0100

    Fix home page

--- recnik/frontend/src/app/pages/home/home.component.ts
+++ recnik/frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-};
 
 @Component({
   selector: 'home',
@@ -14,32 +9,9 @@ const httpOptions = {
 })
 @Injectable({ providedIn: 'root' })
 export class HomeComponent implements OnInit {
-  listOfDeterminants = [];
-  latest = [];
-  changed = [];
-  popular = [];
-  constructor(
-    private primengConfig: PrimeNGConfig,
-    private httpClient: HttpClient,
-  ) {}
-
-  async fetch() {
-    const latest = await this.httpClient
-      .get('api/odrednice/odrednica-latest/')
-      .toPromise();
-    const changed = await this.httpClient
-      .get('api/odrednice/odrednica-changed/')
-      .toPromise();
-    const popular = await this.httpClient
-      .get('api/odrednice/odrednica-popular/')
-      .toPromise();
-    console.log(latest);
-    console.log(changed);
-    console.log(popular);
-  }
+  constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit() {
     this.primengConfig.ripple = true;
-    this.fetch();
   }
 }
--- recnik/frontend/src/app/pages/home/home.module.ts
+++ recnik/frontend/src/app/pages/home/home.module.ts
@@ -1,13 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { FieldsetModule } from 'primeng/fieldset';
 import { HomeComponent } from './home.component';
 import { RenderedOdredniceComponent } from '../rendered-odrednice/rendered-odrednice.component';
 
 @NgModule({
   declarations: [HomeComponent, RenderedOdredniceComponent],
-  imports: [FieldsetModule, HttpClientModule, FormsModule],
+  imports: [FieldsetModule],
   exports: [HomeComponent],
   providers: [],
   bootstrap: [HomeComponent],
