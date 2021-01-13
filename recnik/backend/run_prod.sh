ac5a9bed7a6cfa58817a6eb7c471710f96a264f5
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-01-13 11:49:45 +0100

    Azuriran init skript

--- recnik/backend/run_prod.sh
+++ recnik/backend/run_prod.sh
@@ -5,5 +5,5 @@ touch /app/log/uwsgi.log
 cd /app
 python3 manage.py migrate
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@rsj.rs', 'changeme')" | python3 manage.py shell > /dev/null 2>&1
-python3 manage.py loaddata start_groups start_users
+python3 manage.py loaddata start_groups start_users kvalifikatori vrste_publikacija renderi
 uwsgi --ini /app/config/uwsgi-prod.ini
