6de7c9ad2a49731867bbe010dfbc9582c49fcde4
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-02-08 20:19:48 +0100

    Azuriran Dockerfile da omoguci prijem CLI parametara

--- recnik/backend/run_prod.sh
+++ recnik/backend/run_prod.sh
@@ -6,4 +6,10 @@ cd /app
 python3 manage.py migrate
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@rsj.rs', 'changeme')" | python3 manage.py shell > /dev/null 2>&1
 python3 manage.py loaddata start_groups start_users kvalifikatori vrste_publikacija renderi
-uwsgi --ini /app/config/uwsgi-prod.ini
+
+if [ "$#" -ne 0 ]; then
+  echo "python3 manage.py" "$@"
+  python3 manage.py "$@"
+else
+  uwsgi --ini /app/config/uwsgi-prod.ini
+fi
