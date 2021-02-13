bff8b67099a526d94372243a589b19f85aaf3c87
Ivan Mihajlov <mihajlov.ivan94@gmail.com>
2021-02-13 02:30:43 +0100

    Login forma, auth servisi, UI touch-up

--- korpus-frontend/src/app/app-routing.module.ts
+++ korpus-frontend/src/app/app-routing.module.ts
@@ -1,27 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './services/auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { SelectWordComponent } from './components/select-word/select-word.component';
 import { FormSelectorComponent } from './components/form-selector/form-selector.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: 'tekst/:id',
     component: SelectWordComponent,
+    //canActivate: [AuthGuard]
   },
   {
     path: 'add',
     component: FormSelectorComponent,
+    //canActivate: [AuthGuard]
   },
   {
     path: 'edit/:vrsta/:id',
     component: FormSelectorComponent,
+    //canActivate: [AuthGuard]
   },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
---
+++ korpus-frontend/src/app/services/auth/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private tokenStorageService: TokenStorageService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this.tokenStorageService.getAccessToken();
+    if (token != null)
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+    return next.handle(authReq);
+  }
+}
