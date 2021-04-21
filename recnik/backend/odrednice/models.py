3d1209f1e3a085cc95cc24563bbe7f891a4af5cf
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-04-21 19:22:14 +0200

    Fix #67: redni broj homonima nije obavezan u admin UI

--- recnik/backend/odrednice/models.py
+++ recnik/backend/odrednice/models.py
@@ -95,7 +95,7 @@ class Odrednica(models.Model):
     stanje = models.IntegerField('стање', choices=STANJE_ODREDNICE, default=1)
     opciono_se = models.BooleanField('опционо се', null=True, blank=True)
     version = AutoIncVersionField()
-    rbr_homonima = models.PositiveSmallIntegerField('редни број хомонима', null=True, default=None)
+    rbr_homonima = models.PositiveSmallIntegerField('редни број хомонима', null=True, blank=True, default=None)
     obradjivac = models.ForeignKey(UserProxy, verbose_name='obradjivac', on_delete=models.PROTECT, related_name='odrednice_obradjivaca', default=1)
     redaktor = models.ForeignKey(UserProxy, verbose_name='obradjivac', on_delete=models.PROTECT, related_name='odrednice_redaktora', blank=True, null=True)
     urednik = models.ForeignKey(UserProxy, verbose_name='obradjivac', on_delete=models.PROTECT, related_name='odrednice_urednika', blank=True, null=True)
