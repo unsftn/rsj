c98c35094e9305dbc0cd53adbeb0bfc0a4d94a68
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-04-30 23:06:05 +0200

    STATICFILES_DIRS

--- recnik/backend/recnik/app_settings/base.py
+++ recnik/backend/recnik/app_settings/base.py
@@ -73,6 +73,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
