619383ceef11ccea41851e7510a6212f967110f8
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-04-30 23:13:16 +0200

    change staticfiles root

--- recnik/backend/recnik/app_settings/base.py
+++ recnik/backend/recnik/app_settings/base.py
@@ -71,11 +71,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
