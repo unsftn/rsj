880f21e7948569a3d36805efe5d4fb180d3c735b
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-04-30 22:56:42 +0200

    Dodati static urls

--- recnik/backend/recnik/urls.py
+++ recnik/backend/recnik/urls.py
@@ -43,4 +43,4 @@ urlpatterns = [
     path(r'swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('media/<path:file_path>/', serve_media_file),
-]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
