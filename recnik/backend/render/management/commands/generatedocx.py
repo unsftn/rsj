cf39b129d12689269151165024adf5d9526428ca
Branko Milosavljevic <branko.milosavljevic@gmail.com>
2021-07-01 16:57:39 +0200

    Generisanje word dokumenata

--- recnik/backend/render/management/commands/generatedocx.py
+++ recnik/backend/render/management/commands/generatedocx.py
@@ -13,23 +13,21 @@ class Command(BaseCommand):
     help = 'Generate .docx for RSJ'
 
     def add_arguments(self, parser):
-        parser.add_argument('--slovo', nargs='?', type=str)
-        parser.add_argument('--status', nargs='?', type=int)
+        parser.add_argument('--letter', nargs='?', type=str)
+        parser.add_argument('--type', nargs='?', type=int)
         parser.add_argument('--open', action='store_true', help='Open the file upon creation')
 
     def handle(self, *args, **options):
-        slovo = options.get('slovo')
-        status = options.get('status')
-        if status:
-            log.info(f'Generisanje PDFa za status: {status}')
-        else:
-            log.info('Generisanje PDFa za sve statuse')
+        slovo = options.get('letter')
+        tip = options.get('type')
+        if not tip:
+            tip = 2
         if slovo:
-            log.info(f'Generisanje .docx za slovo {slovo.upper()}')
-            file_name = render_slovo(slovo, 'docx', status)
+            log.info(f'Generisanje DOCX za slovo {slovo.upper()}, tip dokumenta: {tip}')
+            file_name = render_slovo(slovo, 'docx', tip)
         else:
-            log.info(f'Generisanje .docx za ceo recnik')
-            file_name = render_recnik('docx', status)
+            log.info(f'Generisanje DOCX, tip dokumenta: {tip}')
+            file_name = render_recnik('docx', tip)
         if file_name:
             full_path = os.path.join(settings.MEDIA_ROOT, file_name)
             self.stdout.write(self.style.SUCCESS(f'Uspesno generisan .docx: {full_path}'))
