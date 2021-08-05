e5483ea87313656d8c4791177d9963a65c71e0bf
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-08-05 14:50:58 +0200

    Zapoceto ciscenje reci za sortiranje

--- recnik/backend/odrednice/signals.py
+++ recnik/backend/odrednice/signals.py
@@ -1,9 +1,20 @@
-from django.db.models.signals import pre_save
+import unicodedata
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Kvalifikator
+from .models import Kvalifikator, Odrednica
 
 
-@receiver(pre_save, sender=Kvalifikator)
-def sync_kvalifikator_to_memory(sender, instance, raw, using, update_fields, **kwargs):
-    if using == 'default':
+def remove_punctuation(text):
+    cleared_text = ''.join(c for c in text if unicodedata.category(c) in ['Lu', 'Ll', 'Lt', 'Lm', 'Lo', 'NI'])
+    return cleared_text
+
+
+@receiver(post_save, sender=Kvalifikator)
+def sync_kvalifikator_to_memory(sender, instance, created, raw, using, update_fields, **kwargs):
+    if created and using == 'default':
         instance.save(using='memory')
+
+
+@receiver(pre_save, sender=Odrednica)
+def create_sortable_rec(sender, instance, raw, using, update_fields, **kwargs):
+    instance.sortable_rec = remove_punctuation(instance.rec)
