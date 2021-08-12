4bc31d498ea29d552a7f0a33baa424e921808474
Branko Milosavljevic <branko.milosavljevic@gmail.com>
2021-08-12 14:19:04 +0200

    prc 2

--- recnik/backend/render/apps.py
+++ recnik/backend/render/apps.py
@@ -10,4 +10,4 @@ class RenderConfig(AppConfig):
         call_command('migrate', interactive=False, verbosity=0, database='memory')
         call_command('loaddata', 'start_groups', 'start_users', 'vrste_publikacija',
                      'operacije-izmene', 'status_odrednice', 'renderi', verbosity=0, database='memory')
-        # call_command('copycoders', verbosity=0)
+        call_command('copycoders', verbosity=0)
