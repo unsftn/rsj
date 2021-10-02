c1f7662a1cbf2ceb0fc1fd33f97e09ef03557ec5
Branko Milosavljevic <mbranko@uns.ac.rs>
2021-10-02 16:06:50 +0200

    Ciscenje soft hyphena prilikom snimanja odrednice na serverskoj strani

--- recnik/backend/odrednice/signals.py
+++ recnik/backend/odrednice/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Kvalifikator, Odrednica
-from .text import remove_punctuation
+from .models import Kvalifikator, Odrednica, VarijantaOdrednice
+from .text import remove_punctuation, remove_hyphens
 
 
 @receiver(post_save, sender=Kvalifikator)
@@ -11,5 +11,23 @@ def sync_kvalifikator_to_memory(sender, instance, created, raw, using, update_fi
 
 
 @receiver(pre_save, sender=Odrednica)
-def create_sortable_rec(sender, instance, raw, using, update_fields, **kwargs):
+def clean_odrednica(sender, instance, raw, using, update_fields, **kwargs):
+    instance.rec = remove_hyphens(instance.rec)
+    instance.ijekavski = remove_hyphens(instance.ijekavski)
+    instance.prezent = remove_hyphens(instance.prezent)
+    instance.prezent_ij = remove_hyphens(instance.prezent_ij)
+    instance.nastavak = remove_hyphens(instance.nastavak)
+    instance.nastavak_ij = remove_hyphens(instance.nastavak_ij)
+    instance.freetext = remove_hyphens(instance.freetext)
+    instance.info = remove_hyphens(instance.info)
     instance.sortable_rec = remove_punctuation(instance.rec)
+
+
+@receiver(pre_save, sender=VarijantaOdrednice)
+def clean_varijanta(sender, instance, raw, using, update_fields, **kwargs):
+    instance.tekst = remove_hyphens(instance.tekst)
+    instance.ijekavski = remove_hyphens(instance.ijekavski)
+    instance.prezent = remove_hyphens(instance.prezent)
+    instance.prezent_ij = remove_hyphens(instance.prezent_ij)
+    instance.nastavak = remove_hyphens(instance.nastavak)
+    instance.nastavak_ij = remove_hyphens(instance.nastavak_ij)
--- recnik/backend/odrednice/text.py
+++ recnik/backend/odrednice/text.py
@@ -4,3 +4,9 @@ import unicodedata
 def remove_punctuation(text):
     cleared_text = ''.join(c for c in text if unicodedata.category(c) in ['Lu', 'Ll', 'Lt', 'Lm', 'Lo', 'NI', 'Zs'])
     return cleared_text
+
+
+def remove_hyphens(text):
+    if not text:
+        return text
+    return text.replace('\u00ad', '')
