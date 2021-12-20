6ed623aa85fed681747ac496a711a5bcc3d6a2b1
mbranko <mbranko@uns.ac.rs>
2021-12-20 23:11:07 +0100

    Updated ES

--- recnik/backend/run_es_dev.sh
+++ recnik/backend/run_es_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name elastic-recnik --detach docker.elastic.co/elasticsearch/elasticsearch:7.11.1
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name elastic-recnik --detach docker.elastic.co/elasticsearch/elasticsearch:7.16.1
