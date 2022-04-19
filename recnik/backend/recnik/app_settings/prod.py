cc0bb48837085c7a7e5ef91d9357b21a560013ab
mbranko <mbranko@uns.ac.rs>
2022-04-19 08:10:55 +0200

    CSRF trusted origins

--- recnik/backend/recnik/app_settings/prod.py
+++ recnik/backend/recnik/app_settings/prod.py
@@ -35,3 +35,4 @@ EMAIL_HOST_USER = read_or_get('/private/secrets', 'EMAIL_HOST_USER', '******')
 EMAIL_HOST_PASSWORD = read_or_get('/private/secrets', 'EMAIL_HOST_PASSWORD', '**********')
 HEADER_COLOR_SCHEME = read_or_get('/private/secrets', 'HEADER_COLOR_SCHEME', 'blue')
 OBRADA_TUDJIH = read_or_get('/private/secrets', 'OBRADA_TUDJIH', 'False')
+CSRF_TRUSTED_ORIGINS = ['https://*.rsj.rs']
