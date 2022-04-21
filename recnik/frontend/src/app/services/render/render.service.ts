d1d59a7830edf9ef5b2012c09f935b3c9b5fcd3d
mbranko <mbranko@uns.ac.rs>
2022-04-22 00:08:32 +0200

    download link for generated pdfs

--- recnik/frontend/src/app/services/render/render.service.ts
+++ recnik/frontend/src/app/services/render/render.service.ts
@@ -30,17 +30,26 @@ export class RenderService {
     const filename = render.rendered_file.substring(render.rendered_file.lastIndexOf('/') + 1);
     const token = this.tokenStorageService.getAccessToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    this.httpClient.get(render.rendered_file, { headers, responseType: 'blob' as 'json'}).subscribe(
-      (data: any) => {
+    this.httpClient.get(render.rendered_file, { headers, responseType: 'blob' as 'json'}).subscribe({
+      next: (data: any) => {
         const dataType = data.type;
         const binaryData = [];
         binaryData.push(data);
+        const blob = new Blob(binaryData, {type: dataType});
+        const url = window.URL.createObjectURL(blob);
         const downloadLink = document.createElement('a');
-        downloadLink.href = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+        downloadLink.href = url;
         downloadLink.setAttribute('download', filename);
         document.body.appendChild(downloadLink);
-        downloadLink.click();
-        downloadLink.parentNode.removeChild(downloadLink);
-      });
+        downloadLink.dispatchEvent(new MouseEvent(`click`, {bubbles: true, cancelable: true, view: window}));
+        setTimeout(() => {
+          document.body.removeChild(downloadLink);
+          window.URL.revokeObjectURL(url);
+        }, 100);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
 }
