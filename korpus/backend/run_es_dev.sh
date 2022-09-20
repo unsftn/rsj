ce75aa689666df68d993131972aecebff5633eb6
mbranko <mbranko@uns.ac.rs>
2022-09-20 08:45:54 +0200

    Upgrade Elastisearch to 8.4.1

--- korpus/backend/run_es_dev.sh
+++ korpus/backend/run_es_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "xpack.security.enabled=false" --name elastic-korpus --detach elasticsearch:8.3.3
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "xpack.security.enabled=false" --name elastic-korpus --detach elasticsearch:8.4.1
