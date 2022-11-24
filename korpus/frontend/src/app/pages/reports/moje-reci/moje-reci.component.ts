805fae755a8726f5ed64f54081f45967d219c407
mbranko <mbranko@uns.ac.rs>
2022-11-24 07:57:50 +0100

    Naslov izvestaja Moje reci

--- korpus/frontend/src/app/pages/reports/moje-reci/moje-reci.component.ts
+++ korpus/frontend/src/app/pages/reports/moje-reci/moje-reci.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RecService, StatsService } from '../../../services/reci';
+import { TokenStorageService } from '../../../services/auth/token-storage.service';
 
 @Component({
   selector: 'app-moje-reci',
@@ -10,14 +11,17 @@ import { RecService, StatsService } from '../../../services/reci';
 export class MojeReciComponent implements OnInit {
 
   mojeReci: any[] = [];
+  username: string = '';
 
   constructor(
     private router: Router,
     private recService: RecService,
     private statsService: StatsService,
+    private tokenStorageService: TokenStorageService,
   ) { }
 
   ngOnInit(): void {
+    this.username = (this.tokenStorageService.getUser()?.firstName ?? '') + ' ' + (this.tokenStorageService.getUser()?.lastName ?? '');
     this.fetchData();
   }
 
