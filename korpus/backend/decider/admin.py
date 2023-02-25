d7082c4b0cdca3bb8ccdaf67b5c8e8414e0fef85
mbranko <mbranko@uns.ac.rs>
2023-02-25 22:54:14 +0100

    Rec za odluku admin customization

--- korpus/backend/decider/admin.py
+++ korpus/backend/decider/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+class RecZaOdlukuAdmin(admin.ModelAdmin):
+    list_display = ['tekst', 'vreme_odluke', 'donosilac_odluke']
+    list_filter = ['odluka', ('donosilac_odluke', admin.RelatedOnlyFieldListFilter)]
+
 admin.site.register(GenerisaniSpisak)
-admin.site.register(RecZaOdluku)
+admin.site.register(RecZaOdluku, RecZaOdlukuAdmin)
