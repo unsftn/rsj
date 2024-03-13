00f39f71abb0de57d44794f6de6638900cae5efe
mbranko <mbranko@uns.ac.rs>
2024-03-13 09:39:11 +0100

    Korpus backend na Django 5

--- korpus/backend/publikacije/extractor.py
+++ korpus/backend/publikacije/extractor.py
@@ -1,7 +1,8 @@
 import os
 import logging
 from django.conf import settings
-import textract
+from pdfminer.high_level import extract_text
+import docx2txt
 from .models import FajlPublikacije
 
 logger = logging.getLogger(__name__)
@@ -17,13 +18,14 @@ def extract_file(fajl_publikacije):
         return None
     filepath = os.path.join(settings.MEDIA_ROOT, fajl_publikacije.filepath())
     try:
-        text = textract.process(filepath).decode('utf-8')
+        if filepath.endswith('.pdf'):
+            text = extract_text(filepath)
+        elif filepath.endswith('.docx'):
+            text = docx2txt.process(filepath)
+        else:
+            text = ''
     except Exception as e:
         logger.error(f'Greška pri ekstrakciji teksta iz fajla: {filepath}')
         logger.error(e)
         return None
     return text
-    # files = {'file': open(filepath, 'rb')}
-    # response = requests.post('https://extractor.rsj.rs/extract', files=files)
-    # logger.info(f'Status ekstrakcije: {response.status_code}')
-    # return response.json()
