7b1409104922908209f0cfaa0f1db37f8625348f
mbranko <mbranko@uns.ac.rs>
2024-03-24 16:26:19 +0100

    Azurirano pokretanje recnika u kontejneru

--- recnik/backend/run_prod.sh
+++ recnik/backend/run_prod.sh
@@ -5,11 +5,10 @@ cd /app
 
 if [ "$#" -ne 0 ]; then
   echo "python3 manage.py" "$@"
-  python3 manage.py "$@"
+  exec python3 manage.py "$@"
 else
   python3 manage.py migrate
   # python3 manage.py loaddata start_groups start_users kvalifikatori vrste_publikacija operacije-izmene renderi status_odrednice
   python3 manage.py copycoders
-  # uwsgi --ini /app/config/uwsgi-prod.ini
-  gunicorn -b 0.0.0.0:8000 -w 4 --access-logfile /app/log/gunicorn.log recnik.wsgi:application
+  exec gunicorn -b 0.0.0.0:8000 -w 4 --access-logfile /app/log/gunicorn.log recnik.wsgi:application
 fi
