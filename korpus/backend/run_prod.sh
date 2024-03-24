cb346e5e061c2c28aa2145138e045ed56d1cefdd
mbranko <mbranko@uns.ac.rs>
2024-03-24 16:08:44 +0100

    Faster stopping of docker containers

--- korpus/backend/run_prod.sh
+++ korpus/backend/run_prod.sh
@@ -5,8 +5,8 @@ cd /app
 
 if [ "$#" -ne 0 ]; then
   echo "python3 manage.py" "$@"
-  python3 manage.py "$@"
+  exec python3 manage.py "$@"
 else
   python3 manage.py migrate
-  gunicorn -b 0.0.0.0:8000 -w 4 --access-logfile /app/log/gunicorn.log korpus.wsgi:application
+  exec gunicorn -b 0.0.0.0:8000 -w 4 --access-logfile log/gunicorn.log korpus.wsgi:application
 fi
