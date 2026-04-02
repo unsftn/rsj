ff8341a3c5c885acd334ca0fcdc3dea1e4453284
mbranko <mbranko@uns.ac.rs>
2026-04-02 08:28:39 +0200

    Update reindex script

--- search-engine/reindex.sh
+++ search-engine/reindex.sh
@@ -4,6 +4,6 @@ docker run \
   --name cmd-reindex \
   --rm \
   --network recnik \
-  -v ./search_index:/app/search_index \
+  -v $(pwd)/search_index:/app/search_index \
   rsj/search-engine \
   import-all --recnik-host recnik-mysql --korpus-host korpus-mysql --force --output-dir /app/search_index
